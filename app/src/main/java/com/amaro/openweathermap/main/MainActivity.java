b7a26e3f0c6d5a4def46828e322b6323c6584efc
amaro <amarovn@gmail.com>
2016-10-16 10:40:07 -0300

    Criando a MainActivity com Tab

--- app/src/main/java/com/amaro/openweathermap/main/MainActivity.java
+++ app/src/main/java/com/amaro/openweathermap/main/MainActivity.java
@@ -1,6 +1,20 @@
 package com.amaro.openweathermap.main;
 
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.amaro.openweathermap.R;
+import com.amaro.openweathermap.city.CityListFragment;
+import com.amaro.openweathermap.map.MapViewFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by amaro on 15/10/16.
@@ -8,8 +22,63 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
 
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MapViewFragment(), getString(R.string.map));
+        adapter.addFragment(new CityListFragment(),  getString(R.string.favorites));
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 
 }
