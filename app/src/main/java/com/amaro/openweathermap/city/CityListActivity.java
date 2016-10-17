34bf2141a486a58e91d8d02ca6ba3fef4912c956
amaro <amarovn@gmail.com>
2016-10-17 14:38:39 -0300

    Habilitou o botão home na lista de cidades

--- app/src/main/java/com/amaro/openweathermap/city/CityListActivity.java
+++ app/src/main/java/com/amaro/openweathermap/city/CityListActivity.java
@@ -2,6 +2,7 @@ package com.amaro.openweathermap.city;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.amaro.openweathermap.R;
 
@@ -11,10 +12,20 @@ import com.amaro.openweathermap.R;
 
 public class CityListActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.city_list_activity);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setNavigationIcon(R.drawable.partly_cloudy_icon);
+        setSupportActionBar(toolbar);
+
+        //Setando o botão home
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 
